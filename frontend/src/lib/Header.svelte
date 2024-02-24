11cf158aceee46cd0a47e1beac0b119577627e52
CodeMyst <code@myst.rs>
2024-02-24 11:46:48 +0100

    feat: basic styling

--- frontend/src/lib/Header.svelte
+++ frontend/src/lib/Header.svelte
@@ -1,3 +1,19 @@
 <header>
     <h1>filemyst</h1>
+
+    <button>login</button>
 </header>
+
+<style>
+    header {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+    }
+
+    h1 {
+        font-size: var(--fs-large);
+        font-weight: normal;
+        margin: 0;
+    }
+</style>
